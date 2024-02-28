dd5c3889de97f9834e04556c118fd21a7e5b9305
kyungstar <azure0563@gmail.com>
2024-02-29 00:16:21 +0900

    Entity DB Setting

--- src/entity/AppConfig.ts
+++ src/entity/AppConfig.ts
@@ -1,6 +1,14 @@
 
-import { Entity } from "typeorm"
+import {Column, Entity, PrimaryColumn, PrimaryGeneratedColumn} from "typeorm"
 
 @Entity()
 export class ConfigMaster {
+    @PrimaryColumn({ type: "int", generated: "increment" })  // Use "increment" instead of "auto"
+    config_id: number;
+
+    @Column()
+    config_key: string;
+
+    @Column()
+    config_value: string;
 }
