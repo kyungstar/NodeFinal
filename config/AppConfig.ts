566da838002854c95d2822cfdc431722ab21dc6c
kyungstar <azure0563@gmail.com>
2024-03-01 00:31:01 +0900

    새로운 방법 시도중

--- config/AppConfig.ts
+++ config/AppConfig.ts
@@ -1,46 +1,20 @@
-import createEntityManager from "../src/modules/DBHelper";
-import Logger from "../src/modules/Logger";
 import {ConfigMaster} from "../src/entity/AppConfig";
+import createEntityManager from "../src/modules/DBHelper";
 
-export class AppConfig {
-    private static _instance: AppConfig;
-    private static _config: Record<string, any>;
-    private static _isLoaded: boolean = false;
+export default class getConfig {
 
-    private constructor() {}
+    private AppConfig: Map<string, any> = new Map<string, any>();
 
-    public static getInstance(): AppConfig {
-        if (!this._instance) {
-            this._instance = new AppConfig();
-        }
-        return this._instance;
+    public async initConfig() {
+        const entityManager = await createEntityManager();
+
+        const AppConfigList = await entityManager.find(ConfigMaster, {});
+
+        AppConfigList.map(item => {
+            this.AppConfig.set(item.config_key, {
+                config_value: item.config_value
+            });
+        });
     }
 
-    public static async loadAppConfig(): Promise<void> {
-        if (!this._isLoaded) {
-            const entityManager = await createEntityManager();
-
-            // Find config data asynchronously
-            const configList = await entityManager.find(ConfigMaster, {});
-
-            this._config = configList.reduce((acc, config) => {
-                acc[config.config_key] = config.config_value;
-                return acc;
-            }, {} as Record<string, any>);
-
-            this._isLoaded = true;
-        }
-    }
-
-    public static get(key: string): any {
-        if (!this._isLoaded) {
-            throw new Error("AppConfig is not loaded. Call loadAppConfig first.");
-        }
-
-        return this._config?.[key];
-    }
-
-    public static set(key: string, value: any): void {
-        this._config[key] = value;
-    }
 }
